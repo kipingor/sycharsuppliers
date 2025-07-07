f9df8021a19f0093633a738a85d5eed7a119427f
Antony Kipingor <kipingor@gmail.com>
2025-07-07 16:41:59 +0300

    updated statement and added logo

--- resources/views/pdf/statement.blade.php
+++ resources/views/pdf/statement.blade.php
@@ -6,7 +6,7 @@
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
-            font-size: 12px;
+            font-size: 10px;
             margin: 0;
             padding: 2rem;
         }
@@ -40,6 +40,9 @@
 <body>
 
     <div class="header">
+        <div>
+            <img src="http://sycharsuppliers.com/logo.png" style="height: 3em; width: auto" alt="Sychar Suppliers Logo">
+        </div>
         <h2>Water Billing Statement</h2>
         <p><strong>Resident:</strong> {{ $resident->name }}</p>
         <p><strong>Email:</strong> {{ $resident->email }}</p>
@@ -89,6 +92,28 @@
 
     <div class="footer">
         <p>Total Due as of {{ now()->format('Y-m-d') }}: KES {{ number_format($runningBalance, 2) }}</p>
+        <hr style="border-top: 1px solid #bbb; margin-top: 2rem">
+        <p style="margin-bottom: 1rem; margin-top: 2rem; font-size: 10px; color: gray">Services will be billed in accordance with the Service Description. You must pay all undisputed bills in full within 15 days of the billing date, unless otherwise specified under the Terms and Conditions. All bills shall be paid in the currency of the bill</p>
+        <p style=" font-size: 10px; color: gray">Sychar Suppliers retains the right to decline to extend credit and to require that the bill amount be paid immediately or water supply will be discontinued. Sychar Suppliers reserves the right to charge interest of 1.5% per month or the maximum allowable by applicable law, whichever is less, for any undisputed past due bills. You are responsible for all costs of collection, including reasonable attorneys' fees, for any payment default on undisputed bills. In addition, Sychar Suppliers may disconnect supply of water if payment is not received in a timely manner.</p>
+        <hr style="border-top: 1px solid #bbb; margin-top: 2rem">
+        <p style="margin-top: 2rem; text-transform: uppercase; text-align: center; font-size: 10px">ALL CHEQUES PAYABLE TO SYCHAR SUPPLIERS</p>
+        <hr style="border-top: 1px solid #bbb; margin-top: 2rem">
+        <table style="margin-top: 1rem;">
+            <tr>
+                <td>Direct deposite to <span style="font-weight: bold">NCBA Bank</span></td>
+                <td>ACCOUNT NUMBER: <span style="font-weight: bold">1001821276. GALLERIA BRANCH</span></td>
+                <td>PayBill No: <span style="font-weight: bold">880100</span> Account: <span style="font-weight: bold">1001821276</span></td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td style="width: 5%;">
+                    <img src="http://sycharsuppliers.com/icon.png" style="height: 2.5em; width: auto" alt="">
+                </td>
+                <td style="width: 15%;">(+254)0772059705</td>
+                <td style="width: 80%;">sales@sycharsuppliers.com</td>
+            </tr>
+        </table>
     </div>
 
 </body>
